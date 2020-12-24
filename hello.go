692ec234c178cd5c9c17d3f76a4f3f4a8e8ce89b
jmiller <miller.innovairre@gmail.com>
2020-12-23 21:03:16 -0500

    Adds hello world sample code

---
+++ hello.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(fmt.Sprintf("%a - %b"), quote.Go(), "Some fucking dumass")
+}
+
+// turn up
+
+// for what?
+
+// settin gup the repo, 1 sec
+
+// *master
+
+// alright wilheim
+
+// stfu you belgian loser :(git
+
+/// i hate all of this git bs
+// NEW PROJECT
+// happy to see git is still a nightmare even after youve been doing this a while
+
+///jesus only after you start something inccorectly
+/// my life is pain
+// why am i signing up for this again?
